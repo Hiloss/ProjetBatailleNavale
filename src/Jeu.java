ee04f640d9da15085129a0cce86d960b4f227608
Hiloss <brian.bardet0012@gmail.com>
2019-03-20 12:41:57 +0100

    Debut affichage graphique

--- src/Jeu.java
+++ src/Jeu.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 
+import javax.swing.JFrame;
+
 public class Jeu {
 
 	public static void main(String[] args) {
@@ -16,30 +18,40 @@ public class Jeu {
 		//Creation du joueur
 		Joueur j1 = new Joueur(pseudo, taille);
 		
-		//Placement des bateaux
-		for (int i = 0; i < j1.getTabBateau().size(); i++) {
-			boolean stop = false;
-			System.out.println("~~ Placement du bateau: " + j1.getTabBateau().get(i).getNom() + " ~~");
-			while(!stop){
-				System.out.println("Position en X ?");
-				int posX = sc.nextInt();
-				System.out.println("Position en Y ?");
-				int posY = sc.nextInt();
-				Case c = new Case(posX,posY);
-				
-				System.out.println("Orientation du bateau ?");
-				int orien = sc.nextInt();
-
-				stop = j1.placerBateau(j1.getTabBateau().get(i), c, orien);
-				System.out.println(stop);
-			}
-		}
-//		j1.placerBateau(j1.getTabBateau().get(0), new Case(9,9), 1);
-//		j1.placerBateau(j1.getTabBateau().get(1), new Case(2,1), 3);
-//		j1.placerBateau(j1.getTabBateau().get(2), new Case(3,1), 3);
-//		j1.placerBateau(j1.getTabBateau().get(3), new Case(4,1), 3);
-//		j1.placerBateau(j1.getTabBateau().get(4), new Case(5,1), 3);
+//		//Placement des bateaux
+//		for (int i = 0; i < j1.getTabBateau().size(); i++) {
+//			boolean stop = false;
+//			System.out.println("~~ Placement du bateau: " + j1.getTabBateau().get(i).getNom() + " ~~");
+//			while(!stop){
+//				System.out.println("Position en X ?");
+//				int posX = sc.nextInt();
+//				System.out.println("Position en Y ?");
+//				int posY = sc.nextInt();
+//				Case c = new Case(posX,posY);
+//				
+//				System.out.println("Orientation du bateau ?");
+//				int orien = sc.nextInt();
+//
+//				stop = j1.placerBateau(j1.getTabBateau().get(i), c, orien);
+//				System.out.println(stop);
+//			}
+//		}
+		j1.placerBateau(j1.getTabBateau().get(0), new Case(9,9), 1);
+		j1.placerBateau(j1.getTabBateau().get(1), new Case(2,1), 3);
+		j1.placerBateau(j1.getTabBateau().get(2), new Case(3,1), 3);
+		j1.placerBateau(j1.getTabBateau().get(3), new Case(4,1), 3);
+		j1.placerBateau(j1.getTabBateau().get(4), new Case(5,1), 3);
 		
 		System.out.println(j1.getGrilleBateau());
+		
+		//Affichage de la grille
+		JFrame fenetre = new JFrame("Bataille Navale");
+		fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		fenetre.setSize(500, 600);
+		fenetre.setVisible(true);
+		Affichage dessin = new Affichage(j1.getGrilleBateau());
+		fenetre.setContentPane(dessin);
+		fenetre.pack();
+		
 	}
 }
